bfd3d1a781f3831e08624e18e645568097069731
Harsh Pareek <harshpareek@Harshs-MacBook-Air.local>
2024-03-05 21:48:32 +0530

    update on body

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -21,10 +21,7 @@ type HTTPMethodsWithoutBody =
   | "CONNECT"
   | "TRACE"
   | "GET"
-  | "POST"
-  | "PUT"
   | "DELETE"
-  | "PATCH"
   | "OPTIONS"
   | "HEAD"
   | "CONNECT"
@@ -34,18 +31,46 @@ export type EndPoint<
   Params extends ZodSchema,
   RequestBody extends ZodSchema,
   Response extends ZodSchema,
-  SearchParams extends ZodSchema
+  SearchParams extends ZodSchema,
+  Methods extends HTTPMethods = HTTPMethods
 > = {
-  HttpMethod: HTTPMethods;
   path: RouteConfig<Params, SearchParams>;
-  body?: RequestBody;
   response: Response;
-};
+} & (Methods extends HTTPMethodsWithBody
+  ? {
+      HttpMethod: HTTPMethodsWithBody;
+      body: RequestBody;
+    }
+  : {
+      HttpMethod: HTTPMethodsWithoutBody;
+    });
 
-const isMethodWithBody = (
-  method: HTTPMethods
-): method is HTTPMethodsWithBody => {
-  return method === "POST" || method === "PUT" || method === "PATCH";
+/**
+ * @name isHttpMethodWithBody
+ * @description checks if the endpoint has a body, currently we only check for the POST, PUT and PATCH methods
+ * @param endPoint
+ * @returns
+ */
+
+export const isHttpMethodWithBody = <
+  Params extends ZodSchema,
+  RequestBody extends ZodSchema,
+  Response extends ZodSchema,
+  SearchParams extends ZodSchema
+>(
+  endPoint: EndPoint<Params, RequestBody, Response, SearchParams>
+): endPoint is EndPoint<
+  Params,
+  RequestBody,
+  Response,
+  SearchParams,
+  HTTPMethodsWithBody
+> => {
+  return (
+    endPoint.HttpMethod === "POST" ||
+    endPoint.HttpMethod === "PUT" ||
+    endPoint.HttpMethod === "PATCH"
+  );
 };
 
 export const createEndPoint = <
@@ -57,15 +82,15 @@ export const createEndPoint = <
   endPoint: EndPoint<Params, Body, Response, SearchParams>
 ) => {
   return async ({
+    init,
     params,
     body,
-    init,
-  }: EndPointConfig<Params, SearchParams>) => {
+  }: EndPointConfig<Params, Body, typeof endPoint.HttpMethod>) => {
     try {
       const response = await fetch(endPoint.path(params), {
         method: endPoint.HttpMethod,
-        ...(isMethodWithBody(endPoint.HttpMethod) && endPoint.body
-          ? { body: JSON.stringify(endPoint.body?.parse(body)) }
+        ...(isHttpMethodWithBody(endPoint) && body
+          ? { body: JSON.stringify(endPoint.body.parse(body)) }
           : {}),
         ...init,
       });
@@ -87,20 +112,37 @@ export const createEndPoint = <
   };
 };
 
-export type EndPointConfig<Params extends ZodSchema, Body extends ZodSchema> = {
+export type EndPointConfig<
+  Params extends ZodSchema,
+  Body extends ZodSchema,
+  HTTPMethod extends HTTPMethods = HTTPMethods
+> = {
   /**
    * @name params
    * The parameters for the endpoint
    */
   params: input<Params>;
   /**
-   * @name body
-   * The body for the endpoint
-   */
-  body?: input<Body>;
-  /**
    * @name init
    * The request init object for the fetch call
    */
   init?: Omit<RequestInit, "body" | "method">;
-};
+} & (HTTPMethod extends HTTPMethodsWithBody
+  ? { body: input<Body> }
+  : { body: never });
+
+// example
+
+const x = createEndPoint({
+  HttpMethod: "POST",
+  path: createRoute({
+    fn: () => "/",
+    name: "/",
+    options: {
+      internal: true,
+    },
+    paramsSchema: object({}),
+  }),
+  body: object({ name: string() }),
+  response: object({ id: number() }),
+});
